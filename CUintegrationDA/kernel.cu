516b45e2b1665e790a490f6194020a4ab6921006
shridharnator <32407571+shridharnator@users.noreply.github.com>
2020-10-01 11:41:46 +0530

    yeah

--- CUintegrationDA/kernel.cu
+++ CUintegrationDA/kernel.cu
@@ -14,18 +14,18 @@ __device__ double fun(double x) {
 
 
 
-__global__ void sumintegral(double lowbound1, int n, double dx)
+__global__ void sumintegral(double lowbound1, int n, double dx,double *d_c)
 {
 	
-	double c = 0;
+	//double c = 0;
 	for (int i = 0; i < n; i++) {
 		double xi = lowbound1 + (i * dx);
 		double funValue = fun(xi);
 		double rectangleArea = funValue * dx;
-		c += rectangleArea;
+		*d_c += rectangleArea;
 
 	}
-	printf("%f ",c);
+	//printf("%f ",c);
 	
 }
 
@@ -34,7 +34,17 @@ int main() {
 	
 	double lowbound1 = 3;	
 	long int n = 1;	
+	double c;
+	double *d_c;
+	double size = sizeof(double);
+	cudaMalloc((void**)&d_c, size);
+	c = 0;
+	cudaMemcpy(&d_c, &c, size, cudaMemcpyHostToDevice);
+	
 	double dx = (double)lowbound1/n;	
-	 sumintegral << <1, 1 >> > (lowbound1, n, dx);
+	 sumintegral << <1, 1 >> > (lowbound1, n, dx,d_c);
+	 cudaMemcpy(&c, &d_c, size, cudaMemcpyDeviceToHost);
+	 cudaFree(&d_c);
+	 printf("%f", c);
     return 0;
 }
